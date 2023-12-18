2e06cbf1f6dcb6862df65263a2491c6d53fafe42
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2023-12-18 17:16:06 +0000

    created functions to render current day weather and for inputted city and cities available in the history. I also fixed bugs with my app

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 let today = dayjs()
+dayjs.extend(window.dayjs_plugin_customParseFormat)
 
 let weatherIconURL = "https://openweathermap.org/img/wn/"
 let forecastApiURL = "https://api.openweathermap.org/data/2.5/forecast?"
@@ -9,60 +10,167 @@ let units = "&units=metric"
 let searchString = ""
 let queryURL
 
-$("#search-button").on("click", function(event) {
-event.preventDefault()
-searchString = $(".weather-search").val().trim()
-queryURL = forecastApiURL + "q=" + searchString + apiKey + units
 
-fetch(queryURL)
-.then(function (response) {
-    return response.json()
-}).then(function (data) {
-    console.log(data)
-    let weatherForecastData = data.list
+let searchHistory = []
+
+
+
+
+function renderSearchHistory () {
+
+
+    $("#history").empty()
+
     
-    let todaysDate = today.format("DD/MM/YYYY")
 
-    $("#today").empty()
-    let todaysWeatherEL = $("<div>")
-    let cityDateDisplayEl = $("<div>")
-    let todaysWeatherIconCode = data.list[0].weather[0].icon
-    let cityNameEl = $("<h2>")
-    cityNameEl.text(searchString + " " + "(" + todaysDate + ")" + " ")
-    let todaysIconDisplayed = $("<img>")
-    todaysIconDisplayed.attr({"src": weatherIconURL + todaysWeatherIconCode + ".png", "class": "weather-icon", "alt": "weather icon" })
-    // let todaysDateEl = ("<span>")
-    // todaysDateEl.text(todaysDate)
-    let todaysTempEl = $("<p>")
-    todaysTempEl.text("Temp: " + data.list[0].main.temp)
-    let todaysWindEl = $("<p>")
-    todaysWindEl.text("Wind: " + (data.list[0].wind.speed * 3.6).toFixed(2) + " KPH")
-    let todaysHumidityEl = $("<p>")
-    todaysHumidityEl.text("Humidity: " + data.list[0].main.humidity + "%")
+    for (i = 0; i < searchHistory.length; i++){
 
-    cityDateDisplayEl.append(cityNameEl, todaysIconDisplayed)
-    todaysWeatherEL.append(cityDateDisplayEl, todaysTempEl, todaysWindEl, todaysHumidityEl)
-    $("#today").append(todaysWeatherEL)
+        let historyButtons = $("<button>")
 
-    $("#forecast-title").text("5-Day Forecast:")
+        historyButtons.addClass("history-button")
 
-    for(i = 8; i < weatherForecastData.length; i + 8){
-    
+        historyButtons.attr("data-city", searchHistory[i])
+
+        historyButtons.text(searchHistory[i])
+
+        $("#history").append(historyButtons)
+
     }
 
+}
+
+
+function renderCurrentDay (url) {
+
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data)
+            
+            let todaysDate = today.format("DD/MM/YYYY")
+
+        
+            let todaysWeatherEL = $("<div>")
+            let cityDateDisplayEl = $("<div>")
+            let todaysWeatherIconCode = data.list[0].weather[0].icon
+            let cityNameEl = $("<h2>")
+            cityNameEl.text(data.city.name + " " + "(" + todaysDate + ")" + " ")
+            let todaysIconDisplayed = $("<img>")
+            todaysIconDisplayed.attr({"src": weatherIconURL + todaysWeatherIconCode + ".png", "class": "weather-icon", "alt": "weather icon" })
+            let todaysTempEl = $("<p>")
+            todaysTempEl.text("Temp: " + data.list[0].main.temp + " °C")
+            let todaysWindEl = $("<p>")
+            todaysWindEl.text("Wind: " + (data.list[0].wind.speed * 3.6).toFixed(2) + " KPH")
+            let todaysHumidityEl = $("<p>")
+            todaysHumidityEl.text("Humidity: " + data.list[0].main.humidity + "%")
+
+            cityDateDisplayEl.append(cityNameEl, todaysIconDisplayed)
+            todaysWeatherEL.append(cityDateDisplayEl, todaysTempEl, todaysWindEl, todaysHumidityEl)
+            $("#today").append(todaysWeatherEL)
+
+        })
+
+}
+
+function render5DayForecast (url) {
+
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            let weatherForecastData = data.list
+
+            $("#forecast-title").text("5-Day Forecast:")
+
+            for(i = 0; i < weatherForecastData.length; i ++){
+
+                if (weatherForecastData[i].dt_txt.search("09:00:00") != -1){
+                    let forecastWeatherEL = $("<div>")
+
+                    let forecastDateDisplayEl = $("<h3>")
+                    let forecastDate = weatherForecastData[i].dt_txt
+                    const forecastDateArray = forecastDate.split(" ")
+                    forecastDate = dayjs(forecastDateArray[0]).format("DD/MM/YYYY")
+                    forecastDateDisplayEl.text(forecastDate)
+
+                    let forecastIconCode = weatherForecastData[i].weather[0].icon
+                    let forecastIconDisplayed = $("<img>")
+                    forecastIconDisplayed.attr({"src": weatherIconURL + forecastIconCode + ".png", "class": "weather-icon", "alt": "weather icon" })
+
+                    let forecastTempEl = $("<p>")
+                    forecastTempEl.text("Temp: " + weatherForecastData[i].main.temp + " °C")
+
+                    let forecastWindEl = $("<p>")
+                    forecastWindEl.text("Wind: " + (weatherForecastData[i].wind.speed * 3.6).toFixed(2) + " KPH")
+
+                    let forecastHumidityEl = $("<p>")
+                    forecastHumidityEl.text("Humidity: " + weatherForecastData[i].main.humidity + "%")
+
+                    forecastWeatherEL.append(forecastDateDisplayEl, forecastIconDisplayed, forecastTempEl, forecastWindEl, forecastHumidityEl)
+                    $("#forecast").append(forecastWeatherEL)
+                }
+            }
+
+        })
+
+}
+
+
+
+$("#search-button").on("click", function(event) {
+    event.preventDefault()
+
+    $(".search-results").empty()
+
+    searchString = $("#search-input").val().trim()
+
+    if (searchString != ""){
+        const searchStringArr = searchString.split(" ")
+
+    if (searchStringArr.length > 1){
+        for (i = 0; i < searchStringArr.length; i++) {
+            searchStringArr[i] = searchStringArr[i].charAt(0).toUpperCase() + searchStringArr[i].slice(1);
+        
+        }
+    
+        const finalSearchString = searchStringArr.join(" ")
+        if (!searchHistory.includes(finalSearchString)){searchHistory.push(finalSearchString)}
+    } else {
+        if(!searchHistory.includes(searchString)){searchHistory.push(searchString.charAt(0).toUpperCase() + searchString.slice(1))}
+    }
+
+    
+    queryURL = forecastApiURL + "q=" + searchString + apiKey + units
+
+    
+
+        
+    renderCurrentDay(queryURL)
+    render5DayForecast(queryURL)
+    renderSearchHistory()
+    $(".weather-search").val("")
+    }
+
+    
 })
 
-})
+function displayWeather (){
+
+    let citySelected = $(this).attr("data-city")
+
+    let cityQueryUrl = forecastApiURL + "q=" + citySelected + apiKey + units
 
 
+    $(".search-results").empty()
+    renderCurrentDay(cityQueryUrl)
+    render5DayForecast(cityQueryUrl)
 
+}
 
+    $(document).on("click", ".history-button", displayWeather);
 
-
-
-
-
-
+   
 
 
 
